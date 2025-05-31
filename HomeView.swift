7bd37c91de723d1fb860a0fbfffb98c4fcbe4499
BhavaniRoyal <74305507+BhavaniRoyal@users.noreply.github.com>
2025-05-31 16:21:34 -0500

    Initial commit

---
+++ HomeView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("SkillHive")
+                .font(.largeTitle)
+            Text("Book or Offer Peer Tutoring Sessions")
+                .padding()
+        }
+    }
+}
